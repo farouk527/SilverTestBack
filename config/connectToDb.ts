c6dae25be811d711175e2b3f7572cb8766364266
farouk527 <benalayafarouk@gmailcom>
2025-01-26 20:27:17 +0100

    connect to cloud atlas

--- config/connectToDb.ts
+++ config/connectToDb.ts
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
+const uri = "mongodb+srv://benalayafarouk5:OxjPsbUjrDwzO7uT@cluster0.bggbi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 const connectToMongoDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(process.env.MONGO_URL || "");
+    await mongoose.connect(uri || "");
     console.log("Connected To MongoDB ^_^");
   } catch (err) {
     console.error("Connection failed to MongoDB!", err);
